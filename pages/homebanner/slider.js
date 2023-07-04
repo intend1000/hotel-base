c9a30c693917e9ce5f58b5fbd3621542e58625c7
intend1000 <intend1000@gmail.com>
2023-07-04 18:13:46 +0600

    another commit

---
+++ pages/homebanner/slider.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".swiper", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  // const swiper = new Swiper(".swiper", {
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+  //   loop:true,
+  // });
